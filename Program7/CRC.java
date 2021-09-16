ca8992559e83d9338cd525a515a78ebf2b3efdd2
Electromorphous <pstation.474@gmail.com>
2021-09-16 23:14:30 +0530

    final CRC program

--- Program7/CRC.java
+++ Program7/CRC.java
@@ -1,55 +1,53 @@
 import java.util.Scanner;
 
 public class CRC {
-    public String method(String dividend, String divisor) {
+    static String method(String dividend, String divisor) {
+
+        int padding = divisor.length() - 1;
+
         int shift = dividend.length() - divisor.length();
         while (shift >= 0) {
-            // XORing the string
             dividend = Integer.toBinaryString(Integer.parseInt(dividend, 2) ^ Integer.parseInt(divisor, 2) << shift);
             shift = dividend.length() - divisor.length();
         }
-        if (dividend.length() < 16) {
+
+        if (dividend.length() < padding) {
             StringBuilder dividendBuilder = new StringBuilder(dividend);
-            while (dividendBuilder.length() != 16)
-                dividendBuilder.insert(0, "0");
+            dividendBuilder.insert(0, "0".repeat(padding - dividend.length()));
             dividend = dividendBuilder.toString();
         }
-        System.out.println("Dividend = " + dividend);
+
+        System.out.println(" After division : " + dividend);
         return dividend;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String data;
-        String checkSum;
-        String syn;
-        StringBuilder dividend;
-        String receivedData;
-        int padding;
-        String polynomial = "10001000000100001";
-        // Data to be transmitted
-        System.out.println("Enter the data to be encrypted and transmitted");
-        data = sc.next();
-        dividend = new StringBuilder(data);
-        padding = polynomial.length() - 1;
-        // Zero padding of dividend based on the polynomial
-        dividend.append("0".repeat(padding));
-        CRC obj = new CRC();
-        checkSum = obj.method(dividend.toString(), polynomial);
 
-        // Generated codeword
+        StringBuilder dividend;
+        String polynomial = "101010011";
+        int padding = polynomial.length() - 1;
+
+        System.out.println(" Enter the data to encrypt and transmit");
+        String data = sc.next();
+        dividend = new StringBuilder(data);
+
+        dividend.append("0".repeat(padding));
+        String checkSum = method(dividend.toString(), polynomial);
+
         data += checkSum;
-        System.out.println("Sender checkSum = " + checkSum);
-        System.out.println("Codeword transmitted over network = " + data);
-        // Data received by the receiver
-        System.out.println("Enter the data received by the receiver");
-        receivedData = sc.next();
-        syn = obj.method(receivedData, polynomial);
-        // Generated syn bits after checking checkSum
+        System.out.println(" CheckSum = " + checkSum);
+        System.out.println(" Codeword transmitted = " + data);
+
+        System.out.println(" Enter data received");
+        String receivedData = sc.next();
+
+        String syn = method(receivedData, polynomial);
+
         if (Long.parseLong(syn) == 0)
-            System.out.println("No error in data transmission");
+            System.out.println(" No errors");
         else
-            System.out.println("Error in data transmission");
+            System.out.println(" There was error in transmission");
 
         sc.close();
     }
