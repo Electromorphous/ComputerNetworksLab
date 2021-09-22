de1d1e47d9bbc29d044f62111f63742b2edb9946
Electromorphous <pstation.474@gmail.com>
2021-09-22 17:28:03 +0530

    update program 2

--- Program2/count.awk
+++ Program2/count.awk
@@ -1,17 +1,15 @@
 BEGIN {
     #include <stdio.h>
-    d = 0;
     r = 0;
-}
-{
+    d = 0;
+} {
+    if ($1 == "d")
+        d++;
+} {
     if ($1 == "r")
         r++;
 }
-{
-    if ($1 == "d")
-        d++;
-}
 END {
-    printf("Number of packets received = %d \n", r);
-    printf("Number of packets dropped = %d \n", d);
+    printf("\n Packets received = %d \n", r);
+    printf("\n Packets dropped = %d \n", d);
 }
--- Program2/pro.tcl
+++ Program2/pro.tcl
@@ -1,54 +1,52 @@
 set ns [new Simulator]
-set nf [open nam.nam w]
-$ns namtrace-all $nf
 set tf [open trace.tr w]
 $ns trace-all $tf
+set nf [open nam.nam w]
+$ns namtrace-all $nf
 
+set n0 [$ns node]
 set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
-set n6 [$ns node]
 
-$n6 shape box
+$ns duplex-link $n0 $n5 100Mb 1ms DropTail
+$ns duplex-link $n1 $n5 100Mb 1ms DropTail
+$ns duplex-link $n2 $n5 100Mb 1ms DropTail
+$ns duplex-link $n3 $n5 100Mb 1ms DropTail
+$ns duplex-link $n4 $n5 1Mb 1ms DropTail
 
-$ns duplex-link $n1 $n6 1005Mb 1ms DropTail
-$ns duplex-link $n2 $n6 50Mb 1ms DropTail
-$ns duplex-link $n3 $n6 2000Mb 1ms DropTail
-$ns duplex-link $n4 $n6 200Mb 1ms DropTail
-$ns duplex-link $n5 $n6 1Mb 1ms DropTail
+$ns queue-limit $n0 $n5 4
+$ns queue-limit $n2 $n5 3
+$ns queue-limit $n5 $n4 2
+
+set p0 [new Agent/Ping]
+$ns attach-agent $n0 $p0
+$p0 set packetSize_ 50000
+$p0 set interval_ 0.0001
 
 set p1 [new Agent/Ping]
 $ns attach-agent $n1 $p1
-$p1 set packetSize_ 50000
-$p1 set interval_ 0.0001
 
 set p2 [new Agent/Ping]
 $ns attach-agent $n2 $p2
+$p2 set packetSize_ 30000
+$p2 set interval_ 0.00001
 
 set p3 [new Agent/Ping]
 $ns attach-agent $n3 $p3
-$p3 set packetSize_ 30000
-$p3 set interval_ 0.00001
 
 set p4 [new Agent/Ping]
 $ns attach-agent $n4 $p4
 
-set p5 [new Agent/Ping]
-$ns attach-agent $n5 $p5
-
-$ns queue-limit $n1 $n6 4
-$ns queue-limit $n3 $n6 3
-$ns queue-limit $n6 $n5 2
+$ns connect $p0 $p4
+$ns connect $p2 $p3
 
 Agent/Ping instproc recv {from rtt} {
     $self instvar node_
-    puts "node [$node_ id] received reply from $from with $rtt ms rtt"
+    puts "Node [$node_ id] received reply from $from with $rtt ms RTT"
 }
-
-$ns connect $p1 $p5
-$ns connect $p3 $p4
 
 proc finish {} {
     global ns nf tf
@@ -59,51 +57,65 @@ proc finish {} {
     exit 0
 }
 
-$ns at 0.1 "$p1 send"
-$ns at 0.2 "$p1 send"
-$ns at 0.3 "$p1 send"
-$ns at 0.4 "$p1 send"
-$ns at 0.5 "$p1 send"
-$ns at 0.6 "$p1 send"
-$ns at 0.7 "$p1 send"
-$ns at 0.8 "$p1 send"
-$ns at 0.9 "$p1 send"
-$ns at 1.0 "$p1 send"
-$ns at 1.1 "$p1 send"
-$ns at 1.2 "$p1 send"
-$ns at 1.3 "$p1 send"
-$ns at 1.4 "$p1 send"
-$ns at 1.5 "$p1 send"
-$ns at 1.6 "$p1 send"
-$ns at 1.7 "$p1 send"
-$ns at 1.8 "$p1 send"
-$ns at 1.9 "$p1 send"
-$ns at 2.0 "$p1 send"
-$ns at 2.1 "$p1 send"
-$ns at 2.2 "$p1 send"
-$ns at 2.3 "$p1 send"
-$ns at 2.4 "$p1 send"
-$ns at 2.5 "$p1 send"
-$ns at 2.6 "$p1 send"
-$ns at 2.7 "$p1 send"
-$ns at 2.8 "$p1 send"
-$ns at 2.9 "$p1 send"
+$ns at 0.1 "$p0 send"
+$ns at 0.2 "$p0 send"
+$ns at 0.3 "$p0 send"
+$ns at 0.4 "$p0 send"
+$ns at 0.5 "$p0 send"
+$ns at 0.6 "$p0 send"
+$ns at 0.7 "$p0 send"
+$ns at 0.8 "$p0 send"
+$ns at 0.9 "$p0 send"
+$ns at 1.0 "$p0 send"
+$ns at 1.1 "$p0 send"
+$ns at 1.2 "$p0 send"
+$ns at 1.3 "$p0 send"
+$ns at 1.4 "$p0 send"
+$ns at 1.5 "$p0 send"
+$ns at 1.6 "$p0 send"
+$ns at 1.7 "$p0 send"
+$ns at 1.8 "$p0 send"
+$ns at 1.9 "$p0 send"
+$ns at 2.0 "$p0 send"
+$ns at 2.1 "$p0 send"
+$ns at 2.2 "$p0 send"
+$ns at 2.3 "$p0 send"
+$ns at 2.4 "$p0 send"
+$ns at 2.5 "$p0 send"
+$ns at 2.6 "$p0 send"
+$ns at 2.7 "$p0 send"
+$ns at 2.8 "$p0 send"
+$ns at 2.9 "$p0 send"
 
-$ns at 0.1 "$p3 send"
-$ns at 0.2 "$p3 send"
-$ns at 0.3 "$p3 send"
-$ns at 0.4 "$p3 send"
-$ns at 0.5 "$p3 send"
-$ns at 0.6 "$p3 send"
-$ns at 0.7 "$p3 send"
-$ns at 0.8 "$p3 send"
-$ns at 0.9 "$p3 send"
-$ns at 1.0 "$p3 send"
-$ns at 1.1 "$p3 send"
-$ns at 1.2 "$p3 send"
-$ns at 1.3 "$p3 send"
-$ns at 1.4 "$p3 send"
-$ns at 1.5 "$p3 send"
+$ns at 0.1 "$p2 send"
+$ns at 0.2 "$p2 send"
+$ns at 0.3 "$p2 send"
+$ns at 0.4 "$p2 send"
+$ns at 0.5 "$p2 send"
+$ns at 0.6 "$p2 send"
+$ns at 0.7 "$p2 send"
+$ns at 0.8 "$p2 send"
+$ns at 0.9 "$p2 send"
+$ns at 1.0 "$p2 send"
+$ns at 1.1 "$p2 send"
+$ns at 1.2 "$p2 send"
+$ns at 1.3 "$p2 send"
+$ns at 1.4 "$p2 send"
+$ns at 1.5 "$p2 send"
+$ns at 1.6 "$p2 send"
+$ns at 1.7 "$p2 send"
+$ns at 1.8 "$p2 send"
+$ns at 1.9 "$p2 send"
+$ns at 2.0 "$p2 send"
+$ns at 2.1 "$p2 send"
+$ns at 2.2 "$p2 send"
+$ns at 2.3 "$p2 send"
+$ns at 2.4 "$p2 send"
+$ns at 2.5 "$p2 send"
+$ns at 2.6 "$p2 send"
+$ns at 2.7 "$p2 send"
+$ns at 2.8 "$p2 send"
+$ns at 2.9 "$p2 send"
 
 $ns at 3.0 "finish"
 $ns run
